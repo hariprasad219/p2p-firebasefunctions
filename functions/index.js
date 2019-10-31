296437f9b9a4a7f29121d203a79136069a236e8a
Hari Prasad Waddi <wprasad@paypal.com>
2019-10-31 13:40:41 -0700

    Added transactions to the database

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,49 @@
 'use strict';
 
+const Firestore = require('@google-cloud/firestore');
+const projectid = 'helloworld-eca5d';
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-var serviceAccount = require("./helloworld-eca5d-firebase-adminsdk-31zno-b6afd7b432.json");
-
-admin.initializeApp({
-    projectId: 'helloworld-eca5d',
-    credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://helloworld-eca5d.firebaseio.com"
+const firestore = new Firestore({
+  projectId: projectid,
+  timestampsInSnapshots: true,
+  keyFilename: './helloworld-eca5d-firebase-adminsdk-31zno-b6afd7b432.json'
 });
 
 //perform a send money transaction. It involves creating debit leg on the sender side and credit leg on the receiver side. The database that is updated is transactions.
-exports.sendmoney = functions.https.onRequest(async (req, res) => {
-  //Expect sender_id, receiver_id, amount_currency, amount_value, sender_notes and image_url
+exports.sendmoney = functions.https.onRequest((req, res) => {
+  const data = req.body;
+    
+    //Expect sender_id, receiver_id, amount_currency, amount_value, sender_notes and image_url
   
 //  const senderRef = functions.database.ref('/users/' + req.sender_id);
 //  const receiverRef = functions.database.ref('/users/' + req.receiver_id);
   const current_timestamp = Date.now();
   
-  const snapshot = await  admin.database().ref('/transactions').push({
-    payee: req.body.payee,
-    payer: req.body.payer,
-    amountInCents: req.body.amountInCents,
+    // .add() will automatically assign an id
+  return firestore.collection('/transactions').add({
+    payee: data.payee,
+    payer: data.payer,
+    amountInCents: data.amountInCents,
     time_created: current_timestamp,
+  }).then(doc => {
+      console.info('stored new doc id#', doc.id);
+      return res.status(200).send(doc);
+  }).catch(err => {
+      console.error(err);
+      return res.status(404).send({
+        error: 'unable to store',
+        err
+      });
   });
   
 //  //Add the transaction to the sender side and the receiver side transactions
 //  senderRef.collection('transactions').add({id: transactionKey});
 //  receiverRef.collection('transactions').add({id: transactionKey});
   
- // res.status(200).send("Successfully sent money!");
- res.redirect(303, snapshot.ref.toString())
+ //res.status(200).send("Successfully sent money!");
 });
 
 //Simple hello world function to test https
