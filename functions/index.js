416b39ce7adf70c84e2cd990612d06747e0b9ee5
Hari Prasad Waddi <wprasad@paypal.com>
2019-10-31 13:55:56 -0700

    Deleted the secured files

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const Firestore = require('@google-cloud/firestore');
-const projectid = 'helloworld-eca5d';
+const projectid = 'pp-cxp2';
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 const firestore = new Firestore({
   projectId: projectid,
-  timestampsInSnapshots: true,
-  keyFilename: './helloworld-eca5d-firebase-adminsdk-31zno-b6afd7b432.json'
+  timestampsInSnapshots: true
 });
 
 //perform a send money transaction. It involves creating debit leg on the sender side and credit leg on the receiver side. The database that is updated is transactions.
@@ -23,27 +22,23 @@ exports.sendmoney = functions.https.onRequest((req, res) => {
   const current_timestamp = Date.now();
   
     // .add() will automatically assign an id
-  return firestore.collection('/transactions').add({
+firestore.collection('/transactions').add({
     payee: data.payee,
     payer: data.payer,
     amountInCents: data.amountInCents,
     time_created: current_timestamp,
-  }).then(doc => {
-      console.info('stored new doc id#', doc.id);
-      return res.status(200).send(doc);
-  }).catch(err => {
-      console.error(err);
-      return res.status(404).send({
-        error: 'unable to store',
-        err
-      });
   });
   
 //  //Add the transaction to the sender side and the receiver side transactions
 //  senderRef.collection('transactions').add({id: transactionKey});
 //  receiverRef.collection('transactions').add({id: transactionKey});
+    
+var response = {
+  "transaction_id": transactionDoc.id,
+  "message": "Successfully sent money!"
+};
   
- //res.status(200).send("Successfully sent money!");
+ res.status(200).send(response);
 });
 
 //Simple hello world function to test https
